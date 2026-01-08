e2b6912a150c2305db99bcae90ca3237eec84748
Mickida <d.micklausch@me.com>
2026-01-08 12:22:51 +0100

    animation character

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,10 +4,19 @@ class MoveableObject {
   img;
   height = 150;
   width = 100;
+  imgageCache = [];
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imgageCache[path] = img;
+    });
   }
 
   moveRight() {
