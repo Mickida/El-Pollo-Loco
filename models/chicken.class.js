c9eb771e7c1b015ca7d7787124ccd9ce05a39c9b
Mickida <d.micklausch@me.com>
2026-01-09 09:23:32 +0100

    chicken walking

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,9 +2,25 @@ class Chicken extends MoveableObject {
   y = 360;
   height = 70;
   width = 70;
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
 
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imgageCache[path];
+      this.currentImage++;
+    }, 200);
   }
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -5,6 +5,7 @@ class MoveableObject {
   height = 150;
   width = 100;
   imgageCache = [];
+  currentImage = 0;
 
   loadImage(path) {
     this.img = new Image();
