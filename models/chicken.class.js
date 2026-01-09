e6ce54de4843b0cbaff86e9a76cf7bd7b63d6893
Mickida <d.micklausch@me.com>
2026-01-09 13:03:15 +0100

    chicken speed adjusted

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -12,10 +12,12 @@ class Chicken extends MoveableObject {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 500;
     this.loadImages(this.IMAGES_WALKING);
+    this.speed = 0.15 + Math.random() * 0.25;
     this.animate();
   }
 
   animate() {
+    this.moveLeft();
     setInterval(() => {
       let i = this.currentImage % this.IMAGES_WALKING.length;
       let path = this.IMAGES_WALKING[i];
